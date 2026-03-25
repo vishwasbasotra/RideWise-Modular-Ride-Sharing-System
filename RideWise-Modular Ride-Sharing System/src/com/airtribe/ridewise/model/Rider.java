f9fb0ab000bcadc8a9d7ad744ec1f118c2f4f040
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-25 15:32:30 +0530

    Update Rider.java

--- RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/model/Rider.java
+++ RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/model/Rider.java
@@ -1,4 +1,29 @@
 package com.airtribe.ridewise.model;
 
 public class Rider {
+    private String id;
+    private String name;
+    private String location;
+
+    public Rider(String id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
