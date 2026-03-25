5c7903846623bbbebfdc7a08964d204db9d9595c
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-25 15:34:33 +0530

    Update Driver.java

--- RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/model/Driver.java
+++ RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/model/Driver.java
@@ -1,4 +1,39 @@
 package com.airtribe.ridewise.model;
 
 public class Driver {
+    private String id;
+    private String name;
+    private String currentLocation;
+    private boolean available;
+
+    public Driver(String id, String name, String currentLocation, boolean available) {
+        this.id = id;
+        this.name = name;
+        this.currentLocation = currentLocation;
+        this.available = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(String currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
