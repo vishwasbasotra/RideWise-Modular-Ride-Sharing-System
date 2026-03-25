b5b3d69bbe64164efb47911ac4fa9a1a1b259b81
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-25 15:45:13 +0530

    added find driver strategy

--- RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/strategy/RideMatchingStrategy.java
+++ RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/strategy/RideMatchingStrategy.java
@@ -1,4 +1,10 @@
 package com.airtribe.ridewise.strategy;
 
+import com.airtribe.ridewise.model.Driver;
+import com.airtribe.ridewise.model.Rider;
+
+import java.util.List;
+
 public interface RideMatchingStrategy {
+    Driver findDriver(Rider rider, List<Driver> drivers);
 }
