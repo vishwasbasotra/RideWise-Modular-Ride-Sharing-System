6a9410b23d684a26c3eb17c34b871ad7095d0dac
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-25 15:38:05 +0530

    Update Ride.java

--- RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/model/Ride.java
+++ RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/model/Ride.java
@@ -1,4 +1,59 @@
 package com.airtribe.ridewise.model;
 
+import com.airtribe.ridewise.enums.RideStatus;
+
 public class Ride {
+    private String id;
+    private Rider rider;
+    private Driver driver;
+    private long distance;
+    private RideStatus rideStatus;
+
+    public Ride(String id, Rider rider, Driver driver, long distance, RideStatus rideStatus) {
+        this.id = id;
+        this.rider = rider;
+        this.driver = driver;
+        this.distance = distance;
+        this.rideStatus = RideStatus.REQUESTED;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Rider getRider() {
+        return rider;
+    }
+
+    public void setRider(Rider rider) {
+        this.rider = rider;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public long getDistance() {
+        return distance;
+    }
+
+    public void setDistance(long distance) {
+        this.distance = distance;
+    }
+
+    public RideStatus getRideStatus() {
+        return rideStatus;
+    }
+
+    public void setRideStatus(RideStatus rideStatus) {
+        this.rideStatus = rideStatus;
+    }
 }
