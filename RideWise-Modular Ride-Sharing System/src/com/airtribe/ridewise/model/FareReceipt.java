f62de116bbf41762fc0f48e6dfb02ed1c59918f9
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-25 15:42:25 +0530

    Update FareReceipt.java

--- RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/model/FareReceipt.java
+++ RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/model/FareReceipt.java
@@ -1,4 +1,31 @@
 package com.airtribe.ridewise.model;
 
+import java.time.LocalDateTime;
+
 public class FareReceipt {
+    private String rideId;
+    private double amount;
+    private LocalDateTime generatedAr;
+
+    public FareReceipt(String rideId, double amount, LocalDateTime generatedAr) {
+        this.rideId = rideId;
+        this.amount = amount;
+        this.generatedAr = generatedAr;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getGeneratedAr() {
+        return generatedAr;
+    }
+
+    public void setGeneratedAr(LocalDateTime generatedAr) {
+        this.generatedAr = generatedAr;
+    }
 }
