ea5b266d565c799a054b5dd6d370a9f828b10948
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-04-02 16:25:48 +0530

    Update DriverRepository.java

--- RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/repository/DriverRepository.java
+++ RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/repository/DriverRepository.java
@@ -1,4 +1,33 @@
 package com.airtribe.ridewise.repository;
 
+import com.airtribe.ridewise.model.Driver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DriverRepository {
+    private List<Driver> drivers;
+
+    public DriverRepository() {
+        this.drivers = new ArrayList<>();
+    }
+
+    public List<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public String addDriver(Driver driver){
+        drivers.add(driver);
+        return "New driver added successfully";
+    }
+
+    public Driver getDriverById(String id){
+        return drivers.stream().filter(d -> d.getId().equalsIgnoreCase(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Driver not found!!!"));
+    }
+
+    public void deleteDriverById(String id){
+        drivers.removeIf(d -> d.getId().equalsIgnoreCase(id));
+    }
 }
