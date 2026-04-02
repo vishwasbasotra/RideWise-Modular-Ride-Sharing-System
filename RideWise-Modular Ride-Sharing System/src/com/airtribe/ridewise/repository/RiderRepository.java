ee698b31425bf75cd250afeba577840481693a39
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-04-02 16:27:55 +0530

    Update RiderRepository.java

--- RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/repository/RiderRepository.java
+++ RideWise-Modular Ride-Sharing System/src/com/airtribe/ridewise/repository/RiderRepository.java
@@ -1,4 +1,33 @@
 package com.airtribe.ridewise.repository;
 
+import com.airtribe.ridewise.model.Rider;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RiderRepository {
+    private List<Rider> riders;
+
+    public RiderRepository() {
+        this.riders = new ArrayList<>();
+    }
+
+    public List<Rider> getRiders() {
+        return riders;
+    }
+
+    public String addRider(Rider Rider){
+        riders.add(Rider);
+        return "New Rider added successfully";
+    }
+
+    public Rider getRiderById(String id){
+        return riders.stream().filter(r -> r.getId().equalsIgnoreCase(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Rider not found!!!"));
+    }
+
+    public void deleteRiderById(String id){
+        riders.removeIf(r -> r.getId().equalsIgnoreCase(id));
+    }
 }
